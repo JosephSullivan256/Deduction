81ec76bf1de7effaf59177f52cbe0f8d0064e2ce
Joseph Sullivan <josephsullivan256@gmail.com>
2020-09-12 14:15:02 -0700

    fixed formulas being displayed in white against white

--- src/ui/scene.ts
+++ src/ui/scene.ts
@@ -49,18 +49,16 @@ export default class Scene {
         this.ctx.fillStyle = "rgb(255,255,255)";
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
-        let style = this.ctx.fillStyle;
+        let style = "rgb(0,0,0)";
 
         for(let dc of this.deduction_components){
-            
+            this.ctx.fillStyle = style;
+            this.ctx.strokeStyle = style;
             if(this.selected.has(dc)){
                 this.ctx.fillStyle = "rgb(255,0,0)";
                 this.ctx.strokeStyle = "rgb(255,0,0)";
             }
             dc.draw(this.ctx);
-
-            this.ctx.fillStyle = style;
-            this.ctx.strokeStyle = style;
         }
     }
 
@@ -82,6 +80,10 @@ export default class Scene {
     initCanvas() : void {
         this.canvas = document.getElementById("canvas") as HTMLCanvasElement;
         this.ctx = this.canvas.getContext('2d');
+
+        //var scale = window.devicePixelRatio; // Change to 1 on retina screens to see blurry canvas.
+        //this.canvas.width = Math.floor(this.canvas.style.width * scale);
+        //this.canvas.height = Math.floor(size * scale);
 
         this.canvas.addEventListener("click",(ev)=>{
             const rect = this.canvas.getBoundingClientRect()
